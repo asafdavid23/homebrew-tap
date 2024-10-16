50351a0476c2c84dcda0d1859bf47dba69f67a5f
Asaf David <asaf.david@checkmarx.com>
2024-10-16 12:16:07 +0300

    Inital

---
+++ eolctl.rb
@@ -0,0 +1,17 @@
+class YourCliTool < Formula
+    desc "eolctl is Go CLI tool that provide capabillities to query EOL information for varius of products. "
+    homepage "https://github.com/asafdavid23/eolctl"
+    url "https://github.com/asafdavid23/eolctl/archive/v1.0.0.tar.gz"
+    sha256 "8e3ebbb682544cf74c68e1c68be11d3b9415516de94e9b1d4f41bd92e3740fd9"
+    license "MIT"
+  
+    depends_on "go" => :build
+  
+    def install
+      system "go", "build", *std_go_args
+    end
+  
+    test do
+      system "#{bin}/your-cli-tool", "--version"
+    end
+  end
