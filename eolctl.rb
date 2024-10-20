9e3c36377893d5a0704c8de4a5b885c3667cb949
Asaf David <asaf.david@checkmarx.com>
2024-10-20 20:30:40 +0300

    fix workflows

--- eolctl.rb
+++ eolctl.rb
@@ -1,8 +1,8 @@
 class Eolctl < Formula
     desc "eolctl is Go CLI tool that provide capabillities to query EOL information for varius of products. "
     homepage "https://github.com/asafdavid23/eolctl"
-    url "https://github.com/asafdavid23/eolctl/archive/v1.0.1.tar.gz"
-    sha256 "f4d481eea244506e04eb89c29b6c478980a0ab6c5f671a7a7931465fea479f56"
+    url "https://github.com/asafdavid23/eolctl/archive/v1.0.0.tar.gz"
+    sha256 "01f8561ebedefaae995a5a605177b0b267bbaf2c276115e5b0c9e4cb608d82b7"
     license "MIT"
   
     depends_on "go" => :build
