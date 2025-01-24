b1dfe618bcb077fc9988965e800654aae4364b19
Asaf David <asaf.david@earnix.com>
2025-01-24 23:09:11 +0200

    Update vpc-cidr-manager to v1.0.3

--- vpc-cidr-manager.rb
+++ vpc-cidr-manager.rb
@@ -1,8 +1,8 @@
 class VpcCidrManager < Formula
     desc "A Simple CLI tool to manage AWS VPC CIDR block reservations stored in DynamoDB."
     homepage "https://github.com/asafdavid23/vpc-cidr-manager"
-    url "https://github.com/asafdavid23/vpc-cidr-manager/archive/v1.0.2.tar.gz"
-    sha256 "a9e849cdcb0486be90f24fab12a83ed6016638a20e4b00f4cd27ee575f76ac6d"
+    url "https://github.com/asafdavid23/vpc-cidr-manager/archive/v1.0.3.tar.gz"
+    sha256 "b86d1af35d7e59c4e6d9c07f0385b9eba5612d72241840084a4dfce42ff439b8"
     license "MIT"
   
   depends_on "go" => :build
